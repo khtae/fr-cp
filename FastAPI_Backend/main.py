2a809307236850763f647ad4ccb081a6eb368676
Akmanar <125309655+khtae@users.noreply.github.com>
2024-06-06 02:30:55 +0500

    Update main.py

--- FastAPI_Backend/main.py
+++ FastAPI_Backend/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from model import recommend,output_recommended_recipes
 
 
-dataset=pd.read_csv('../Data/dataset.csv',compression='gzip')
+dataset=pd.read_csv('https://www.kaggle.com/datasets/irkaal/foodcom-recipes-and-reviews?select=recipes.csv',compression='gzip')
 
 app = FastAPI()
 
